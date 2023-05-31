a90eb2ee48ef025a4dd7c8d634c1f8b0345c53df
satvikamahapatra <82950175+satvikamahapatra@users.noreply.github.com>
2023-05-31 22:38:47 +0530

    Delete dashboardPageUpdate.php

--- dashboardPageUpdate.php
+++
@@ -1,7 +0,0 @@
-<?php
-$second=$_GET['update'];
-if($second=='update'){
-    header("Location: http://localhost/SQL-Injection/SQL-Injection-master/SQL%20injection%20UI/UI%20design/updatePage/updateStudentDetail.html");
-    exit();
-}
-?>
